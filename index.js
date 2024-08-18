e16abe8c4b6386f2b9f5a49758f5c2424e30675a
Henrique Freitas de Lima <henriquefreitasl28@gmail.com>
2024-08-18 17:43:59 -0300

    Implement post delete feature

--- index.js
+++ index.js
@@ -4,23 +4,56 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+//Middlewares
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//Array to store posts
+const posts = [];
+
+//Post class to create a Post Object
+class Post {
+    constructor(postTitle, postContent) {
+        this.postTitle = postTitle;
+        this.postContent = postContent;
+        this.postDate = new Date();
+    }
+}
+
+//Load Page
 app.get("/", (req, res)=>{
-    res.render("index.ejs");
-})
-
-app.post("/create", (req, res)=>{
-
-    posts.push(req.body);
-
-    console.log(posts);
     res.render("index.ejs", {posts});
 })
 
+//Creating a Post Object and storing it in array posts[]
+app.post("/createPost", (req, res) => {
+
+    var post = new Post(req.body.postTitle, req.body.postContent);
+
+    posts.push(post);
+
+    console.log(posts);
+    res.redirect("/");
+});
+
+//Deleting a post using splice. The delete button has a hiddenvalue that is the post index inside the array
+app.post("/delete", (req, res)=>{
+
+    posts.splice(req.body.postIndex, 1);
+
+    res.redirect("/");
+})
+
+// app.post("/editPost", (req, res)=>{
+
+//     console.log(req.body.postIndex);
+//     posts[req.body.postIndex].postTitle = req.body.postTitle;
+//     posts[req.body.postIndex].postContent = req.body.postContent;
+
+//     res.redirect("/");
+// })
+
+//Set Server
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}.`);
 })
-
-const posts = [];
