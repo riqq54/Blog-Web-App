8ea3bc1e360e953b1e0dda94d3a3812c20eb8b72
Henrique Freitas de Lima <henriquefreitasl28@gmail.com>
2024-08-18 00:17:23 -0300

    First try at post creation feature

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res)=>{
     res.render("index.ejs");
+})
+
+app.post("/create", (req, res)=>{
+
+    posts.push(req.body);
+
+    console.log(posts);
+    res.render("index.ejs", {posts});
 })
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}.`);
 })
+
+const posts = [];
